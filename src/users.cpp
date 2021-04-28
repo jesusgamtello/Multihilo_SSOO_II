847ec688a235e258271994f38a6e7c25bc0384ff
jesusgamtello <Jesus.Gamero@alu.uclm.es>
2021-04-29 01:51:26 +0200

    creacion clase petition y modificacion users

--- src/users.cpp
+++ src/users.cpp
@@ -1,97 +1,64 @@
-
-#include <iostream>
-#include <mutex>
-#include <vector>
-#include <thread>
-#include <algorithm>
-#include <functional>
-#include <condition_variable>
-#include <iostream>
-
-/*class User{
+#include<iostream>
+class User{
 
     private:
         int id;
-        std::string random_word;
         std::string type;
         int limit_words;
         int credits;
         
 
     public:
-        User(int id, std::string random_word,std::string type,int limit_words,int credits){
+        User(int id, std::string type,int limit_words,int credits){
             this -> id = id;
-            this -> random_word = random_word;
             this -> type = type;
             this -> limit_words = limit_words;
             this -> credits = credits;
         }
+        //getters
         int get_id(){return id;}
-        std::string get_random_word(){return random_word;}
         std::string get_type(){return type;}
+        int get_limit_words(){return limit_words;}
+        int get_credits(){return credits;}
+        //setters
         void set_id(int id){
             this->id = id;
         }
-        void set_random_word(std::string random_word){
-            this->random_word = random_word;
+        void set_type(std::string type){
+            this->type = type;
         }
-
-        
-};*/
-class User{
-
-    private:
-        int id;
-        std::string random_word;
-
-    public:
-        User(int id, std::string random_word){
-            this -> id = id;
-            this -> random_word = random_word;
+        void set_limit_words(int limit_words){
+            this->limit_words = limit_words;
         }
-        int get_id(){return id;}
-        std::string get_random_word(){return random_word;}
-        void set_id(int id){
-            this->id = id;
-        }
-        void set_random_word(std::string random_word){
-            this->random_word = random_word;
-        }
-
-
-};
-
-class Ilimited : public User{
-    public:
-        Ilimited(int id, std::string random_word):User(id,random_word){
-           
-        }
-};
-
-class Free : public User{
-    private:
-        int number_of_words;
-    public:
-        Free(int id, std::string random_word, int number_of_words):User(id,random_word){
-            this -> number_of_words = number_of_words;
-        }
-        int get_number_of_words(){return number_of_words;}
-        void set_number_of_words(int number_of_words){
-            this -> number_of_words = number_of_words;
-        }
-        
-};
-
-class Premium_credits : public User{
-    private:
-        int credits;
-    public:
-        Premium_credits(int id, std::string random_word, int credits):User(id,random_word){
+        void set_credits(int credits){
             this->credits = credits;
         }
-        int get_credits(){return credits;}
-        void set_credis(int credits){
-            this -> credits = credits;
-        }
+    
+
         
 };
+class Petition{
+
+    private:
+        User user;
+        std::string random_word;
+       
+        
+
+    public:
+        Petition(User user, std::string random_word):user(user),random_word(random_word){}
+          
+        //getters
+        User get_user(){return user;}
+        std::string get_random_word(){return random_word;}
+        //setters
+        void set_id(User user){
+            this->user = user;
+        }
+        void set_random_word(std::string random_word){
+            this->random_word = random_word;
+        }
+
+        
+};
+
