fe7a7daed849f7d0340b91f6aafbde44e37c1695
pedro <pedrinni14>
2021-04-29 03:26:37 +0200

    llllllllll

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,24 @@
 #include <condition_variable>
 #include <iostream>
 
-#define CLIENTS 10
+#define CLIENTS 5
 #define TOKENS 500
 #define WORDS_FREE 500
 
 //std::queue<User> queue_users;
-std::vector<User> v_users;
+std::vector<Petition> v_petition;
 
 int contador = 0;
 int buffer=0;
 std::mutex m;
-void create_objects(){
-    
-    
-}
+void select_random_word()
+    {
+        std::vector<std::string> v = {"actitud", "velocidad", "oro", "Cuál", "perdón", "Qué", "hola", "Cuando"};
+        srand(time(NULL));
+        int n_random = rand() % (v.size());
+
+        std::cout << v[n_random] << std::endl;
+    }
 /*User random_type(int id ){
     int n_random = 1 + rand() % 3;
     if (n_random==1){
@@ -42,22 +46,27 @@ User random_type(int id){
     
     int n_random = 1 + rand() % 3;
     if (n_random==1){
-        User u(id, "", "f", WORDS_FREE, -1);
+        User u(id, "f", WORDS_FREE, -1);
         return u;
     }
     else if(n_random==2){
-        User u(id, "", "l", -1, TOKENS);
+        User u(id, "l", -1, TOKENS);
         return u;
     }
     else{
-        User u(id,"","p",-1,-1); 
+        User u(id,"p",-1,-1); 
         return u;
     }
 
 }
 void create_client(int id){
     User u = random_type(id);
-    v_users.push_back(u);
+    Petition p(u,"hola");
+
+    m.lock();
+    v_petition.push_back(p);
+    m.unlock();
+
 }
 void attend(int id){
     std::cout<<"ATENDIENDO..."<< id<< std::endl;
@@ -69,31 +78,61 @@ void attend(int id){
 }
 int search_type(std::string type){
  
-    for (int i = 0; i < v_users.size(); i++)
+    for (int i = 0; i < v_petition.size(); i++)
     {
-        if (v_users[i].get_type() == type)
+        if (v_petition[i].get_user().get_type() == type)
         {
-            return  v_users[i].get_id();
+            return  i;
         }
     }
     return -1;
 }
-void initialize_search(){
-    int id;
+void initialize_search()
+{
+    int position;
 
-    while(1){
-        int n_random = 1 + rand() % (11 - 1);
-        if (n_random <= 2)
+    while (1)
+    {
+        if (v_petition.size() != 0)
         {
-            id=search_type("f");
-        }
-        else if (n_random > 2)
-        {
-            if (n_random % 2 == 0)
+            int n_random = 1 + rand() % (11 - 1);
+            if (n_random <= 2)
             {
+                    position = search_type("f");
+                if(position!=-1){
+                    Petition p = v_petition[position];
+                    std::cout << v_petition.size() << std::endl;
+                    std::cout << "SOY CLIENTE FREE ID: " << p.get_user().get_id() << std::endl;
+                    v_petition.erase(v_petition.begin()+position);
+                    std::cout << v_petition.size() << std::endl;
+                }
             }
-            else
+            else if (n_random > 2)
             {
+                if (n_random % 2 == 0)
+                {
+                        position = search_type("l");
+                    if (position!=-1){
+                        Petition p = v_petition[position];
+                        std::cout << v_petition.size() << std::endl;
+                        std::cout << "SOY CLIENTE LIMITADO ID: " << p.get_user().get_id() << std::endl;
+                        v_petition.erase(v_petition.begin()+position);
+                        std::cout << v_petition.size() << std::endl;
+
+                    }
+                }
+                else
+                {
+                        position = search_type("p");
+                    if(position!=-1){
+                        Petition p = v_petition[position];
+                        std::cout << v_petition.size() << std::endl;
+                        std::cout << "SOY CLIENTE ILIMITADO PREMIUM ID: " << p.get_user().get_id() << std::endl;
+                        v_petition.erase(v_petition.begin()+position);
+                        std::cout << v_petition.size() << std::endl;
+
+                    }
+                }
             }
         }
     }
@@ -109,7 +148,10 @@ int main(){
     std::thread searcher(initialize_search);
     searcher.join();
 
-    create_objects();
+    //for(int i = 0; i < v_petition.size(); i++){
+   //     std::cout << v_petition[i].get_user().get_id() << std::endl;
+    //}
+
 
 
 }
